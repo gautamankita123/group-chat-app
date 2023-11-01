db8dc1155c0dbb50514d408cc21eff651f412085
Ankitagau <gautamanku06@gmail.com>
2023-11-01 20:00:17 +0530

    Expense dhinesh

--- group-chat-app-3014abd2be075b56fb15ffa61dd4c27286d7030b/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('expence', 'root', 'mysql@1234', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
